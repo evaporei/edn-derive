ebbe02a9a0d78a4d982f34699ad84c6468c3d8b3
Otavio Pace <otaviopp8@gmail.com>
2020-08-16 08:59:36 -0300

    Use edn_rs::to_string new public function

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,7 +1,6 @@
-use edn_derive::Serialize as SerializeEdn;
-use edn_rs::Serialize;
+use edn_derive::Serialize;
 
-#[derive(SerializeEdn)]
+#[derive(Serialize)]
 pub struct Person {
     name: String,
     age: usize,
@@ -12,5 +11,8 @@ fn main() {
         name: "joana".to_string(),
         age: 290000,
     };
-    assert_eq!(person.serialize(), "{ :name \"joana\", :age 290000, }");
+    assert_eq!(
+        edn_rs::to_string(person),
+        "{ :name \"joana\", :age 290000, }"
+    );
 }
