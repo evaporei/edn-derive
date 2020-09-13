7c0d849e41e3d1c4368fddb48739204eb5507b32
Otavio Pace <otaviopp8@gmail.com>
2020-09-13 16:26:11 -0300

    refactor: extract struct deserialization into a function

--- src/deserialize.rs
+++ src/deserialize.rs
@@ -2,25 +2,11 @@ use crate::enums::{generate_variant_deserialization, get_enum_variants};
 use crate::structs::{generate_field_deserialization, get_struct_fields};
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
-use syn::{Data, Ident};
+use syn::{Data, DataStruct, Ident};
 
 pub fn expand(type_name: &Ident, data: &Data) -> TokenStream2 {
     match data {
-        Data::Struct(ref data_struct) => {
-            let struct_fields = get_struct_fields(data_struct);
-
-            let deserialized_fields = generate_field_deserialization(&struct_fields);
-
-            quote! {
-                impl edn_rs::Deserialize for #type_name {
-                    fn deserialize(edn: &edn_rs::Edn) -> Result<Self, edn_rs::EdnError> {
-                        Ok(Self {
-                            #deserialized_fields
-                        })
-                    }
-                }
-            }
-        }
+        Data::Struct(ref data_struct) => expand_struct(type_name, data_struct),
         Data::Enum(ref data_enum) => {
             let enum_variants = get_enum_variants(data_enum);
 
@@ -55,5 +41,21 @@ pub fn expand(type_name: &Ident, data: &Data) -> TokenStream2 {
             }
         }
         _ => unimplemented!(),
+    }
+}
+
+fn expand_struct(struct_name: &Ident, data_struct: &DataStruct) -> TokenStream2 {
+    let struct_fields = get_struct_fields(data_struct);
+
+    let deserialized_fields = generate_field_deserialization(&struct_fields);
+
+    quote! {
+        impl edn_rs::Deserialize for #struct_name {
+            fn deserialize(edn: &edn_rs::Edn) -> Result<Self, edn_rs::EdnError> {
+                Ok(Self {
+                    #deserialized_fields
+                })
+            }
+        }
     }
 }
